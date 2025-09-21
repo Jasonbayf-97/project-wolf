0003ea3ddde10c86da48597dfcc5f10474dae0d9
Jasonbayford97 <jasonbayford97@gmail.com>
2025-09-21 15:56:46 +0200

    About me page section completed might need a little bit more of work

---
+++ project-routing/src/shared/models/about-me.model.ts
@@ -0,0 +1,12 @@
+export const ABOUT_ME = {
+  title: 'About Me',
+  diploma: 'Diploma in Sport Management Science',
+  degree: 'BSc Information Technology',
+  honours: 'BSc (Hons) Software Engineering',
+  experience: '3+ years of Angular ',
+  work: 'Investec',
+  paragraphOne: `I am a passionate technologist and business enthusiast with a diverse background spanning sport management, information technology, and software engineering. My academic journey began with a diploma in Sport Management Science, fueling my love for the world of sports and competition. I built on that foundation with a BSc in Information Technology and an honours BSc in Software Engineering, equipping myself with a drive for innovation and problem-solving in today’s fast-paced digital landscape.`,
+  paragraphTwo: `Business and entrepreneurship inspire me every day, and I am actively working toward pursuing my MBA to further sharpen my skills. Beyond my professional ambitions, I have a lifelong love for sports—having proudly played professional rugby for the junior Sharks—which continues today in my passion for golf, gym sessions, and exploring the bush.`,
+  paragraphThree: `Yet above all, family is at the heart of everything I do. Whether it’s through personal projects, work challenges, or simply enjoying great moments together, I aim to innovate and create solutions that make a genuine difference in the real world.`,
+  paragraphFour: `When I’m not working or on the course, you’ll find me enjoying gaming, spending quality time with loved ones, and always seeking out new adventures.`,
+};
