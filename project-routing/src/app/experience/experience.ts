d272af8386658a5e3e1ed29d976ad05990c5fed1
Jasonbayford97 <jasonbayford97@gmail.com>
2025-09-22 19:45:01 +0200

    Implemented experience page

--- project-routing/src/app/experience/experience.ts
+++ project-routing/src/app/experience/experience.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { EXPERIENCE } from '../../shared/models/experience.model';
 
 @Component({
   selector: 'app-experience',
   imports: [],
   templateUrl: './experience.html',
-  styleUrl: './experience.scss'
+  styleUrl: './experience.scss',
 })
 export class Experience {
-
+  experience = EXPERIENCE;
 }
---
+++ project-routing/src/shared/models/experience.model.ts
@@ -0,0 +1,60 @@
+export const EXPERIENCE = {
+  title: 'Experience',
+  jobs: [
+    {
+      company: 'Xcontent',
+      year: 2023,
+      description:
+        'Worked at XContent as a frontend developer, contributing to projects for Redefine Properties.',
+    },
+    {
+      company: 'Investec',
+      year: '2024 - Present',
+      description:
+        'I am currently a frontend developer at Investec, where I build corporate cash management systems for intermediaries. Additionally, I am exploring pursuing an MBA to broaden my business knowledge and enhance my professional growth.',
+    },
+  ],
+  techStackTitle: 'Tech Stack',
+  techStack: [
+    {
+      name: 'Azure-900',
+      level: 'Certified',
+    },
+    {
+      name: 'Angular',
+      level: 'Intermediate',
+    },
+    {
+      name: 'HTML',
+      level: 'Experienced',
+    },
+    {
+      name: 'SCSS',
+      level: 'Experienced',
+    },
+    {
+      name: 'JavaScript',
+      level: 'Basic',
+    },
+    {
+      name: 'Bootstrap',
+      level: 'Intermediate',
+    },
+    {
+      name: 'Material UI',
+      level: 'Intermediate',
+    },
+    {
+      name: 'TypeScript',
+      level: 'Basic',
+    },
+    {
+      name: 'Springboot',
+      level: 'Beginner*',
+    },
+    {
+      name: '.NET',
+      level: 'Beginner*',
+    },
+  ],
+};
