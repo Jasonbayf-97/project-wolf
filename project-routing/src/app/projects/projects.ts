f472f4185abb4f063f2ff8f6bd82b8f5e7c338e5
Jasonbayford97 <jasonbayford97@gmail.com>
2025-09-24 10:11:47 +0200

    Implemented projects

--- project-routing/src/app/projects/projects.ts
+++ project-routing/src/app/projects/projects.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BtnLabels } from '../../shared/models/projects.modal';
 
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.html',
-  styleUrl: './projects.scss'
+  styleUrl: './projects.scss',
 })
 export class Projects {
-
+  btnLabels = BtnLabels;
 }
---
+++ project-routing/src/shared/models/projects.modal.ts
@@ -0,0 +1,25 @@
+export const PROJECTS = [
+  {
+    title: 'Project 1',
+    description: 'Description for project 1',
+    githubLink: '',
+    liveDemoLink: '',
+  },
+  {
+    title: 'Project 2',
+    description: 'Description for project 2',
+    githubLink: '',
+    liveDemoLink: '',
+  },
+  {
+    title: 'Project 3',
+    description: 'Description for project 3',
+    githubLink: '',
+    liveDemoLink: '',
+  },
+];
+
+export const BtnLabels = {
+  github: 'Github',
+  liveDemo: 'Live Demo',
+};
