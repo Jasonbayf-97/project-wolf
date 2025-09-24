4a578d0bc6a30930fca7d4d2b794dc854c0959b9
Jasonbayford97 <jasonbayford97@gmail.com>
2025-09-24 10:41:02 +0200

    Implemented a status check to checking which projects are live

--- project-routing/src/shared/components/card/card.ts
+++ project-routing/src/shared/components/card/card.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './card.scss',
 })
 export class Card {
+  @Input() status: boolean = false;
   @Input() Title: String = '';
   @Input() Description: String = '';
   @Input() buttonLabels = { github: '', liveDemo: '' };
